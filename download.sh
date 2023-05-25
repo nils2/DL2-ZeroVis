025d30a86295f7df3c2131525efea57441318011
Gergely Papp <gergely.papp@renyi.hu>
2023-05-25 06:13:35 +0000

    download script works

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 benchmark_root=src
-emb_root=fromage_inf/fromage_model/
+emb_root=src/fromage_inf/fromage_model
 
 mkdir -p $benchmark_root
 mkdir -p $emb_root
@@ -7,7 +9,7 @@ mkdir -p $emb_root
 if [ ! -f $emb_root/cc3m_embeddings.pkl ]
 then
     echo "Downloading CC3M embedding pickle file.."
-    gdown --fuzzy https://drive.google.com/u/0/uc?id=1wMojZNqEwApNlsCZVvSgQVtZLgbeLoKi&export=download
+    python src/drive_download.py https://drive.google.com/u/0/uc?id=1wMojZNqEwApNlsCZVvSgQVtZLgbeLoKi
     mv cc3m_embeddings.pkl $emb_root/cc3m_embeddings.pkl
     echo "Done."
 else
@@ -17,7 +19,7 @@ fi
 if [ ! -f $benchmark_root/benchmark/cities/washington.jpg ]
 then
     echo "Downloading Visual Relations Dataset.."
-    gdown --fuzzy https://drive.google.com/u/0/uc?id=1a8992QI5SpX0tu_HZVE6pZNBTtBSGnp-&export=download
+    python src/drive_download.py https://drive.google.com/u/0/uc?id=1a8992QI5SpX0tu_HZVE6pZNBTtBSGnp-
     unzip benchmark.zip
     mv benchmark $benchmark_root/benchmark
     rm benchmark.zip
---
+++ src/drive_download.py
@@ -0,0 +1,9 @@
+# This is a SYNHCRONOUS download for bash script
+# otherwise bash script would skip over download command
+# right after starting it
+
+import gdown
+import sys
+
+url = sys.argv[1]
+gdown.download(url, quiet=False, fuzzy=True)
